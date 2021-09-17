37ec440bc80fcc3f10e3562d659efc42ff2909df
Shaya Selincourt <sselincourt@gmail.com>
2021-09-16 22:31:55 -0700

    Added a feature to read from document.txt

--- README.md
+++ README.md
@@ -1,3 +1,5 @@
 # COSC 499 Individual GitHub Exercise
 
-A little CLI node.js application that takes user input and writes it to a .txt file. It can also read the .txt file.
+A little CLI node.js application that takes user input and writes it to a .txt file and can read the .txt file.
+
+To run, type `npm run dev` in the root directory.
--- index.js
+++ index.js
@@ -1,8 +1,20 @@
+const fs = require("fs");
+
+/**
+ * Takes a given input string and writes it to
+ * @param {String} input
+ */
 const printer = (input) => {
-  let fs = require("fs");
-  fs.writeFile("document.txt", input, function (err) {
+  fs.writeFile("document.txt", "utf8", input, function (err) {
     if (err) return console.log(err);
     console.log("[ %c" + input, "color: red", "] printed to document.txt");
+  });
+};
+
+const scanner = () => {
+  fs.readFile("document.txt", "utf8", function (err, data) {
+    if (err) return console.log(err);
+    console.log(data);
   });
 };
 
@@ -15,5 +27,8 @@ while (!correctPrompt) {
     correctPrompt = true;
     input = prompt("Enter a text input: ");
     printer(input);
+  } else if (input === "s") {
+    correctPrompt = true;
+    scanner();
   }
 }
