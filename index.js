912449ef61a821cbd96228572452ee13a88b6de1
Shaya Selincourt <sselincourt@gmail.com>
2021-09-16 21:56:07 -0700

    implemented function to print user input to a .txt

--- index.js
+++ index.js
@@ -1,3 +1,11 @@
+const printer = (input) => {
+  let fs = require("fs");
+  fs.writeFile("document.txt", input, function (err) {
+    if (err) return console.log(err);
+    console.log("[ %c" + input, "color: red", "] printed to document.txt");
+  });
+};
+
 const prompt = require("prompt-sync")();
 const input = prompt("Enter input: ");
-console.log(`Your input: ${input}`);
+printer(input);
