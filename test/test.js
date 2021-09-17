aa07e9201f09dce8e90cc73fd3cf4a4200f9e300
Shaya Selincourt <sselincourt@gmail.com>
2021-09-17 00:15:07 -0700

    Created more test cases

--- test/test.js
+++ test/test.js
@@ -10,7 +10,40 @@ describe("printing", () => {
       let filename = "test/test.txt";
       tools.printer(input, filename);
       let data = tools.scanner(filename);
-      assert.equal("test", data);
+      assert.equal(input, data);
+    });
+  });
+});
+describe("printing nothing", () => {
+  describe("scanning", () => {
+    it("should print '' to test.txt then scan it back", () => {
+      let input = "";
+      let filename = "test/test.txt";
+      tools.printer(input, filename);
+      let data = tools.scanner(filename);
+      assert.equal(input, data);
+    });
+  });
+});
+describe("printing a large string", () => {
+  describe("scanning", () => {
+    it("should print input from lorem.txt to test.txt then scan it back", () => {
+      let input = tools.scanner("test/lorem.txt");
+      let filename = "test/test.txt";
+      tools.printer(input, filename);
+      let data = tools.scanner(filename);
+      assert.equal(input, data);
+    });
+  });
+});
+describe("printing undefined", () => {
+  describe("scanning", () => {
+    it("should print input from lorem.txt to test.txt then scan it back", () => {
+      let input = undefined;
+      let filename = "test/test.txt";
+      tools.printer(input, filename);
+      let data = tools.scanner(filename);
+      assert.equal(input, data);
     });
   });
 });
